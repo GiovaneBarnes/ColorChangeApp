95e2aecba5b455d4afa6a92736d5c35fcc7faef1
Giovane Barnes <64332692+GiovaneBarnes@users.noreply.github.com>
2021-07-07 22:49:33 -0400

    Added Folders For Projects
    Models, View

---
+++ ColorChangeApp/Model/ColorTool.swift
@@ -0,0 +1,22 @@
+//
+//  ColorTool.swift
+//  ColorChangeApp
+//
+//  Created by Giovane Barnes on 7/7/21.
+//  Copyright © 2021 Giovane Barnes. All rights reserved.
+//
+
+import UIKit
+
+public class ColorTool{
+    
+    public func createRandomColor() -> UIColor
+    {
+        let redPercent : CGFloat = CGFloat(arc4random() % 256 / 255)
+        let greenPercent : CGFloat = CGFloat(arc4random() % 256 / 255)
+        let bluePercent : CGFloat = CGFloat(arc4random() % 256 / 255)
+    
+        return UIColor(red: redPercent, green: greenPercent, blue: bluePercent, alpha: CGFloat(100))
+    }
+    
+}
